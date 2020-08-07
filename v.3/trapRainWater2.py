370ae300693db49836ba29c353fd40fce64c11e7
김지안 <john0316@interpark.com>
2020-08-07 18:55:21 +0900

    adding comment

--- v.3/trapRainWater2.py
+++ v.3/trapRainWater2.py
@@ -29,17 +29,17 @@ class Solution(object):
 
         for i in range(m):
             for j in range(n):
-                if i == 0 or j == 0 or i == m - 1 or j == n - 1:
+                if i == 0 or j == 0 or i == m - 1 or j == n - 1: #테두리면은...
                     heapq.heappush(heap, (heightMap[i][j], i, j))
                     visited[i][j] = 1
 
         result = 0
-        while heap:
-            height, i, j = heapq.heappop(heap)
+        while heap: #힙의 원소가 없을때까지 반복
+            height, i, j = heapq.heappop(heap)  #테두리 값부터 작은것부터 빠진다.
             for x, y in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
-                if 0 <= x < m and 0 <= y < n and not visited[x][y]:
-                    result += max(0, height - heightMap[x][y])
-                    heapq.heappush(heap, (max(heightMap[x][y], height), x, y))
+                if 0 <= x < m and 0 <= y < n and not visited[x][y]: #메트릭스 안에 있으면서 방문 안한 것을 탐색
+                    result += max(0, height - heightMap[x][y]) #양각인지 음각인지 구별
+                    heapq.heappush(heap, (max(heightMap[x][y], height), x, y)) #탐색할 대상에 새롭게 추가 , #수면을 찾기 위해 max값을 업데이트해가며...
                     visited[x][y] = 1
         return result
 
