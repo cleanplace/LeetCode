e7b7a55fac9180de5b0fe0bc2b453d03b666acad
김지안 <john0316@interpark.com>
2020-08-14 17:19:49 +0900

    solved

--- v.3/largestNumber.py
+++ v.3/largestNumber.py
@@ -1,3 +1,18 @@
+"""
+179. Largest Number
+
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+* Example 1:
+Input: [10,2]
+Output: "210"
+
+* Example 2:
+Input: [3,30,34,5,9]
+Output: "9534330"
+
+"""
+
 class LargerNumKey(str):
     def __lt__(x, y):
         return x + y > y + x
@@ -6,3 +21,11 @@ class Solution:
     def largestNumber(self, nums):
         largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
         return '0' if largest_num[0] == '0' else largest_num
+
+if __name__ == "__main__":
+    input=[3, 30, 34, 5, 9]
+
+    s = Solution()
+    result = s.largestNumber(input)
+
+    print(result)
