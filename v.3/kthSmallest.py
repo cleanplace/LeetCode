53ea148cfee9d6afd12afd70b2efff1e553b75f8
김지안 <john0316@interpark.com>
2020-08-14 12:31:11 +0900

    solved

---
+++ v.3/kthSmallest.py
@@ -0,0 +1,34 @@
+"""
+378. Kth Smallest Element in a Sorted Matrix
+"""
+
+import heapq
+
+class Solution:
+    def kthSmallest(self, matrix, k):
+        N = len(matrix)
+
+        element = 0
+        minHeap = []
+
+        for r in range(min(k, N)):
+            minHeap.append((matrix[r][0], r, 0))
+
+        heapq.heapify(minHeap)
+
+        while k:
+            element, r, c = heapq.heappop(minHeap)
+
+            if c < N - 1:
+                heapq.heappush(minHeap, (matrix[r][c + 1], r, c + 1))
+
+            k -= 1
+
+        return element
+
+if __name__ == "__main__":
+    a = [[1, 5, 9], [10, 11, 13], [12, 13, 15]]
+    s = Solution()
+    result = s.kthSmallest(a,8)
+
+    print(result)
