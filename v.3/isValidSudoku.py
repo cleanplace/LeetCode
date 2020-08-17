c4c39ddaa8f2e6db23da88cf5b05a07ecbce1f7c
kim jung mi <marine_k@naver.com>
2020-08-18 01:13:18 +0900

    다른 풀이 추가

--- v.3/isValidSudoku.py
+++ v.3/isValidSudoku.py
@@ -24,6 +24,14 @@ class Solution:
         return True
 
 
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = [[x for x in y if x != '.'] for y in board]
+        col = [[x for x in y if x != '.'] for y in zip(*board)]
+        pal = [[board[i+m][j+n] for m in range(3) for n in range(3) if board[i+m][j+n] != '.'] for i in (0, 3, 6) for j in (0, 3, 6)]
+        return all(len(set(x)) == len(x) for x in (*row, *col, *pal))
+
+
 #rows[0] = {3:1,5:1,7:1}
 #rows[1] = {1:1,6:1,8:1,}
 #
