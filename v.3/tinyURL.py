3e5524d948fd36a365d84119c4b108eff852fd9e
kim jung mi <marine_k@naver.com>
2020-08-20 23:10:34 +0900

    sloved

---
+++ v.3/tinyURL.py
@@ -0,0 +1,34 @@
+import random
+import string
+
+class Codec:
+
+    alphabet = string.ascii_letters + '0123456789'
+
+    def __init__(self):
+        self.url2code = {}
+        self.code2url = {}
+
+    def encode(self, longUrl):
+        while longUrl not in self.url2code:
+            code = ''.join(random.choice(Codec.alphabet) for _ in range(6))
+            if code not in self.code2url:
+                self.code2url[code] = longUrl
+                self.url2code[longUrl] = code
+        return 'http://tinyurl.com/' + self.url2code[longUrl]
+
+    def decode(self, shortUrl):
+        return self.code2url[shortUrl[-6:]]
+
+
+
+if __name__ == "__main__":
+    c = Codec()
+    longUrl='https://leetcode.com/problems/design-tinyurl'
+
+
+    en = c.encode(longUrl)
+    de = c.decode(en)
+
+    print(en)
+    print(de)
