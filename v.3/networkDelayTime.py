ea56b16049518f0d127182e7ea0e4001300e4619
kim jung mi <marine_k@naver.com>
2020-08-28 01:36:01 +0900

    다른 알고리즘 추가

--- v.3/networkDelayTime.py
+++ v.3/networkDelayTime.py
@@ -1,8 +1,9 @@
 import collections
 import heapq
 
+#Dijkstra algorithm
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+    def networkDelayTime(self, times, N, K) :
 
         q, t, adj = [(0, K)], {}, collections.defaultdict(list)
 
@@ -17,3 +18,41 @@ class Solution:
                     heapq.heappush(q, (time + w, v))
 
         return max(t.values()) if len(t) == N else -1
+
+
+# Bellman–Ford algorithm
+class Solution:
+    def networkDelayTime(self, times, N, K):
+        distance = [float("inf") for _ in range(N)]
+        distance[K-1] = 0
+        for _ in range(N-1):
+            for u, v, w in times:
+                if distance[u-1] + w < distance[v-1]:
+                    distance[v-1] = distance[u-1] + w
+        return max(distance) if max(distance) < float("inf") else -1
+
+
+
+#  Floyd Warshall
+class Solution:
+    def networkDelayTime(self, times, N, K):
+        elapsedTimeMatrix = [[float("inf") for _ in range(N)] for _ in range(N)]
+        for u, v, w in times:
+            elapsedTimeMatrix[u - 1][v - 1] = w
+        for i in range(N):                      #   Assigning 0 to the diagonal cells
+            elapsedTimeMatrix[i][i] = 0
+        for k in range(N):
+            for i in range(N):
+                for j in range(N):
+                    elapsedTimeMatrix[i][j] = min(elapsedTimeMatrix[i][j], elapsedTimeMatrix[i][k] + elapsedTimeMatrix[k][j])
+        mx = max(elapsedTimeMatrix[K - 1])
+        return mx if mx < float("inf") else -1
+
+
+if __name__ == "__main__":
+    times = [[2, 1, 1], [2, 3, 1], [3, 4, 1]]
+    N = 4
+    K = 2
+
+    s = Solution()
+    print(s.networkDelayTime(times,N,K))
