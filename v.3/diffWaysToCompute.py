5fa514b2d4312018bbe542f7e04e94e7e5d36ee0
김지안 <john0316@interpark.com>
2020-09-16 15:36:47 +0900

    solved

---
+++ v.3/diffWaysToCompute.py
@@ -0,0 +1,24 @@
+class Solution:
+    def diffWaysToCompute(self, input):
+        return self.helper(input, {})
+
+    def helper(self, input, seen):
+        if input in seen:
+            return seen[input]
+        if input.isnumeric():
+            return [int(input)]
+        res = []
+        for i, c in enumerate(input):
+            if c in "+-*":
+                res += [l + r if c == "+" else l - r if c == "-" else l * r
+                        for l in self.helper(input[:i], seen)
+                        for r in self.helper(input[i + 1:], seen)]
+        seen[input] = res
+        return res
+
+if __name__ == "__main__":
+    #input = "2-1-1"
+    input = "2*3-4*5"
+
+    s = Solution()
+    print(s.diffWaysToCompute(input))
