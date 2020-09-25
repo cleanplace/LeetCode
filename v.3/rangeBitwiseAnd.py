ebb9db53d10e4738fdaf3080c1234ecf1531ebb3
김지안 <john0316@interpark.com>
2020-09-25 15:22:32 +0900

    solved

--- v.3/rangeBitwiseAnd.py
+++ v.3/rangeBitwiseAnd.py
@@ -1,3 +1,4 @@
+#O(1)/O(1)
 class Solution:
     def rangeBitwiseAnd(self, m: int, n: int) -> int:
         i = 0
@@ -6,3 +7,7 @@ class Solution:
             n >>= 1
             i += 1
         return n << i
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.rangeBitwiseAnd(5,7))
