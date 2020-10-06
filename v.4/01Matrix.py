58abb54a271821a67ada69405125169595985e4b
김지안 <john0316@interpark.com>
2020-10-06 17:00:46 +0900

    solved

---
+++ v.4/01Matrix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
+        rows = len(matrix[0])
+        cols = len(matrix)
+
+
+
+
+if __name__ == "__main__":
+
