636587402ce7d3b99129ad9fd40e9d85fd73653a
kim jung mi <marine_k@naver.com>
2020-11-16 00:00:31 +0900

    sloved

---
+++ v.4/validSerialization.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSerialization(self, preorder: str) -> bool:
+        slots = 1
+
+        for node in preorder.split(','):
+            slots -= 1
+
+            if slots < 0:
+                return False
+
+            if node != '#':
+                slots += 2
+
+        return slots == 0
+
+
+if __name__ == "__main__":
+    #input = "9,3,4,#,#,1,#,#,2,#,6,#,#"
+    input="9,#,#,1"
+
+    s = Solution()
+    print(s.isValidSerialization(input))
