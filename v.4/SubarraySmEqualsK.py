43ceaa0965bafdce7d67a5623494f395b212c2c4
kim jung mi <marine_k@naver.com>
2021-05-12 21:46:48 +0900

    sloved

--- v.4/SubarraySmEqualsK.py
+++ v.4/SubarraySmEqualsK.py
@@ -1,27 +1,34 @@
+"""
+560. Subarray Sum Equals K
+"""
+
 class Solution(object):
     def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        count = 0
-        sums = 0
-        d = dict()
-        d[0] = 1
 
-        for i in range(len(nums)):
-            sums += nums[i]
-            count += d.get(sums - k, 0)
-            d[sums] = d.get(sums, 0) + 1
+        tmp_dict = {0: 1}
+        sum = 0
 
-        return (count)
+        ans = 0
+
+        for i, n in enumerate(nums):
+            sum += nums[i]
+            if sum - k in tmp_dict:
+                ans += tmp_dict[sum - k]
+            if sum in tmp_dict:
+                tmp_dict[sum] += 1
+            else:
+                tmp_dict[sum] = 1
+
+        return ans
 
 
 if __name__ == "__main__":
 
-    nums = [1, 1, 1]
-    k = 2
+    # nums = [1, 1, 1]
+    # k = 2
+
+    nums = [1, 2, 3]
+    k = 3
 
     s = Solution()
     print(s.subarraySum(nums,k))
