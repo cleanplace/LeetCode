f47cd2297df98c806846654c9d0811ed29f85c22
kim jung mi <marine_k@naver.com>
2022-05-12 04:19:46 +0900

    sloved

--- v.5/mergeTrees.py
+++ v.5/mergeTrees.py
@@ -1,10 +1,10 @@
-class Solution(object):
-    def mergeTrees(self, t1, t2):
-        if t1 and t2:
-            root = TreeNode(t1.val + t2.val)
-            root.left = self.mergeTrees(t1.left, t2.left)
-            root.right = self.mergeTrees(t1.right, t2.right)
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root1 and root2:
+            root = TreeNode(root1.val + root2.val)
+            root.left = self.mergeTrees(root1.left, root2.left)
+            root.right = self.mergeTrees(root1.right, root2.right)
             return root
 
         else:
-            return t1 or t2
+            return root1 or root2
