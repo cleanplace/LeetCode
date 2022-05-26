2cbd253bbf8e6968e5de210128c2c5458f6196dc
kim jung mi <marine_k@naver.com>
2022-05-26 19:21:57 +0900

    sloved

---
+++ v.5/numJewelsInStones.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        jewel = set(jewels)
+
+        return sum(stone in jewel for stone in stones)
