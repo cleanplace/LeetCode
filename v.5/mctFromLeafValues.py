d54d236e85bf8af33e3b46e7ef1b6709d50a741a
kim jung mi <marine_k@naver.com>
2022-06-09 19:28:59 +0900

    sloved

---
+++ v.5/mctFromLeafValues.py
@@ -0,0 +1,18 @@
+def mctFromLeafValues(A):
+    res = 0
+    stack = [float('inf')]
+
+    for a in A:
+        while stack[-1] <= a:
+            mid = stack.pop()
+            res += mid * min(stack[-1], a)
+        stack.append(a)
+
+    while len(stack) > 2:
+        res += stack.pop() * stack[-1]
+    return res
+
+if __name__ == "__main__":
+    A = [6, 2, 4]
+
+    print(mctFromLeafValues(A))
