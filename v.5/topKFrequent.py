7a0c7725803747ca6c17312a87d509e0f9163642
kim jung mi <marine_k@naver.com>
2022-06-16 19:15:34 +0900

    sloved

---
+++ v.5/topKFrequent.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        return [x for x, y in collections.Counter(nums).most_common(k)]
